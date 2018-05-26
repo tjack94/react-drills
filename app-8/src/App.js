d2b00899965e3a70774dd6a73c86249d5dc1ab3b
Taylor Jackson <tjack94@hotmail.com>
2018-05-26 09:09:28 -0600

    Completed App-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state= {
+      tatooine:''
+    }
+  }
+  componentDidMount(){
+    axios.get('https://swapi.co/api/planets/1').then(response => {
+      this.setState( { tatooine: response.data } )
+    })
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Name: {this.state.tatooine["name"]}</h1>
+        <h1>Climate: {this.state.tatooine.climate}</h1>
+        <h1>Population: {this.state.tatooine.population}</h1>
       </div>
     );
   }
