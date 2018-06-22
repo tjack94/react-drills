835baf5065a8b755a01dde431b13c332e7530e89
Taylor Jackson <tjack94@hotmail.com>
2018-06-21 21:03:30 -0600

    app 9 complete

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './home';
+import Signup from './signup';
+import Details from './details';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+					<Link to="/">home</Link>
+					<Link to="/signup">Sign Up</Link>
+					<Link to="/details">Details </Link>
+					<Switch>
+						<Route path exact="/" component={Home} />
+						<Route path="/signup" component={Signup} />
+						<Route path="/details" component={Details} />
+					</Switch>
+				
+			</div>
+		);
+	}
 }
 
 export default App;
